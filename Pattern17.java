4125961bf1fc9ef4a21562f0c291611ef36685b2
Sonalisahu346 <65212240+Sonalisahu346@users.noreply.github.com>
2020-09-29 19:08:17 +0530

    Add files via upload

---
+++ Pattern17.java
@@ -0,0 +1,15 @@
+package prgs7;
+
+public class Pattern17 {
+	public static void main(String args[])
+	{
+		for(int i=5;i<=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
