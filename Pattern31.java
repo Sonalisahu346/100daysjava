5fe5b4558378e43efc244bbf001104c5909ec7ca
Sonalisahu346 <65212240+Sonalisahu346@users.noreply.github.com>
2020-10-03 18:47:46 +0530

    Add files via upload

---
+++ Pattern31.java
@@ -0,0 +1,18 @@
+package prgs7;
+
+public class Pattern31 {
+	public static void main(String[] args) {
+		int n = 5; // 5 rows
+		
+		for(int i=n; i >= 1 ; i--)// i = 5, 4,3 rows
+		{
+			for(int j= n-1; j >= i ; j--)// j=4 >= 3	
+				System.out.print(" "); // 0,1,2 -> spaces
+			
+			for(int k=1; k <= i; k++) // k=1 <= 3 ->t 
+				System.out.print(k); // 5,4,3  -> *
+			
+			System.out.println();			
+		}
+	}
+}
---
+++ Pattern35.java
@@ -0,0 +1,21 @@
+package prgs7;
+
+public class Pattern35 {
+	public static void main(String[] args) {
+		int stars = 1;
+		int n = 5;  // rows
+		int space = n-1; // space = 4
+		
+		for(int i=1; i<=n;i++) // 5 rows
+		{
+			for(int j=space; j>=i;j--)// 4>=1			
+				System.out.print(" ");
+			
+			for(int k =1;k<=stars;k++) // 1<=3
+				System.out.print("*"); // *
+			
+			System.out.println();
+				stars=stars+2;//  3,5,7,9,11
+		}	
+	}
+}
