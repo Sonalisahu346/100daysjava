a5b52061c797339436a731a8973914fb2257dfda
Sonalisahu346 <65212240+Sonalisahu346@users.noreply.github.com>
2020-10-26 18:04:32 +0530

    Add files via upload

---
+++ Pattern92.java
@@ -0,0 +1,32 @@
+package Patterns2;
+
+public class Pattern92 {
+	public static void main(String[] args) {
+		int n = 9; 
+		int x = n/2+1 ;  
+		int p = 1; 
+		
+		for(int i=1; i<=n ; i++) 
+		{
+			for(int j=1; j <=n; j++) 
+			{
+				if(j == x || j == n-x+1 )  
+				  System.out.print((char)(p+64)); 
+				else
+			     System.out.print(" ");
+			}
+			System.out.println();
+			if(i<=n/2) 
+			{
+				p=p+1;   
+				x = x-1; 
+			}
+			else 
+			{
+				p=p-1; 
+				x=x+1; 
+			}
+		}
+		
+	}
+}
